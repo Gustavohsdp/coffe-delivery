a05f35759c887f24f6609906e8c6d7c372073bc6
Gustavo Henrique <gustavohsdp@gmail.com>
2023-04-12 17:22:25 -0300

    feat: :sparkles: create component remove button

---
+++ src/components/RemoveButton.tsx
@@ -0,0 +1,18 @@
+import { Trash2 } from "lucide-react";
+import { ButtonHTMLAttributes } from "react";
+
+interface RemoveButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+}
+
+export function RemoveButton({ title, ...props }: RemoveButtonProps) {
+  return (
+    <button
+      className="bg-brown-300 hover:bg-brown-400 rounded-md h-8 min-w-[92px] font-normal text-brown-700 text-xs flex flex-row items-center gap-1 justify-center"
+      {...props}
+    >
+      <Trash2 size={16} className="text-purple-400" />
+      {title}
+    </button>
+  );
+}
