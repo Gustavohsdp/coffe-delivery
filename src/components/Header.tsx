f113a7d9dfc21917b3b6dc1236bf93c12e639034
Gustavo Henrique <gustavohsdp@gmail.com>
2023-04-14 10:14:32 -0300

    feat: :sparkles: Add function handleHomePage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,24 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { CartButton } from "./CartButton";
 import { Location } from "./Location";
 import { Logo } from "./Logo";
 
 export function Header() {
+  const router = useRouter();
+
+  function handleHomePage() {
+    router.push("/");
+  }
+
   return (
     <div className="w-full  py-8 fixed backdrop-blur-xl z-10">
       <div className="flex flex-row items-center justify-between mx-auto max-w-[1120px]">
-        <Logo />
+        <button onClick={handleHomePage}>
+          {" "}
+          <Logo />
+        </button>
 
         <div className="flex flex-row gap-3 items-center">
           <Location city="Itabira" state="MG" />
