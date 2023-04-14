e338d084e94a61e0b18d5e64bbdf5cabe13cb41c
Gustavo Henrique <gustavohsdp@gmail.com>
2023-04-14 16:49:03 -0300

    feat: :sparkles: update mock coffes

--- src/mocks/coffes.ts
+++ src/mocks/coffes.ts
@@ -6,6 +6,7 @@ export const COFFES = [
     description: "O tradicional café feito com água quente e grãos moídos",
     value: "9,90",
     image: "/assets/images/Expresso.png",
+    quantity: 0,
   },
   {
     id: 2,
@@ -14,6 +15,7 @@ export const COFFES = [
     description: "Expresso diluído, menos intenso que o tradicional",
     value: "9,90",
     image: "/assets/images/Americano.png",
+    quantity: 0,
   },
   {
     id: 3,
@@ -22,6 +24,7 @@ export const COFFES = [
     description: "Café expresso tradicional com espuma cremosa",
     value: "9,90",
     image: "/assets/images/Expresso Cremoso.png",
+    quantity: 0,
   },
   {
     id: 4,
@@ -30,6 +33,7 @@ export const COFFES = [
     description: "Bebida preparada com café expresso e cubos de gelo",
     value: "9,90",
     image: "/assets/images/Café Gelado.png",
+    quantity: 0,
   },
   {
     id: 5,
@@ -38,6 +42,7 @@ export const COFFES = [
     description: "Meio a meio de expresso tradicional com leite vaporizado",
     value: "9,90",
     image: "/assets/images/Café com Leite.png",
+    quantity: 0,
   },
   {
     id: 6,
@@ -47,6 +52,7 @@ export const COFFES = [
       "Uma dose de café expresso com o dobro de leite e espuma cremosa",
     value: "9,90",
     image: "/assets/images/Latte.png",
+    quantity: 0,
   },
   {
     id: 7,
@@ -56,6 +62,7 @@ export const COFFES = [
       "Bebida com canela feita de doses iguais de café, leite e espuma",
     value: "9,90",
     image: "/assets/images/Capuccino.png",
+    quantity: 0,
   },
   {
     id: 8,
@@ -65,6 +72,7 @@ export const COFFES = [
       "Café expresso misturado com um pouco de leite quente e espuma",
     value: "9,90",
     image: "/assets/images/Macchiato.png",
+    quantity: 0,
   },
   {
     id: 9,
@@ -73,6 +81,7 @@ export const COFFES = [
     description: "Café expresso com calda de chocolate, pouco leite e espuma",
     value: "9,90",
     image: "/assets/images/Mochaccino.png",
+    quantity: 0,
   },
   {
     id: 10,
@@ -81,6 +90,7 @@ export const COFFES = [
     description: "Bebida feita com chocolate dissolvido no leite quente e café",
     value: "9,90",
     image: "/assets/images/Chocolate Quente.png",
+    quantity: 0,
   },
   {
     id: 11,
@@ -90,6 +100,7 @@ export const COFFES = [
       "Drink gelado de café expresso com rum, creme de leite e hortelã",
     value: "9,90",
     image: "/assets/images/Cubano.png",
+    quantity: 0,
   },
   {
     id: 12,
@@ -98,6 +109,7 @@ export const COFFES = [
     description: "Bebida adocicada preparada com café e leite de coco",
     value: "9,90",
     image: "/assets/images/Havaiano.png",
+    quantity: 0,
   },
   {
     id: 13,
@@ -106,6 +118,7 @@ export const COFFES = [
     description: "Bebida preparada com grãos de café árabe e especiarias",
     value: "9,90",
     image: "/assets/images/Árabe.png",
+    quantity: 0,
   },
   {
     id: 14,
@@ -114,5 +127,6 @@ export const COFFES = [
     description: "Bebida a base de café, uísque irlandês, açúcar e chantilly",
     value: "9,90",
     image: "/assets/images/Irlandês.png",
+    quantity: 0,
   },
 ];
